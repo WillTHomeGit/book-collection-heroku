661d892ddf73cddbddec0992ac59e6c38b6ae987
WilliamT-byte <williamt2023@tamu.edu>
2026-02-03 04:01:41 +0000

    Fix user_books view specs for associations

--- spec/views/user_books/new.html.erb_spec.rb
+++ spec/views/user_books/new.html.erb_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "user_books/new", type: :view do
   before(:each) do
+    user = User.create!(username: "Test User")
+    book = Book.create!(
+      title: "Test Book",
+      author: "Author",
+      price: 9.99,
+      published_date: Date.new(2024, 1, 1)
+    )
+
     assign(:user_book, UserBook.new(
-      user_id: 1,
-      book_id: 1
+      user: user,
+      book: book
     ))
   end
 
@@ -12,9 +20,8 @@ RSpec.describe "user_books/new", type: :view do
     render
 
     assert_select "form[action=?][method=?]", user_books_path, "post" do
-      assert_select "input[name=?]", "user_book[user_id]"
-
-      assert_select "input[name=?]", "user_book[book_id]"
+      assert_select "select[name=?]", "user_book[user_id]"
+      assert_select "select[name=?]", "user_book[book_id]"
     end
   end
 end
